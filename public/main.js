26ce3e5f459f54292bce639edaffd1f70317f6b7
Daniel Hyres <danhyres@gmail.com>
2022-11-09 12:35:56 -0500

    added date to title header

--- public/main.js
+++ public/main.js
@@ -11,23 +11,18 @@ function picOfDay(){
   fetch(`https://api.nasa.gov/planetary/apod?api_key=aYmOC1DdrReJnOfgEjvWUgf8Du8wXR1VATNKFiRj`)
     .then( res => res.json() )
     .then( data => {
-      console.log(data)
+      // console.log(data)
 
       let dateOg = data.date.split('-') //original date format split into an array
-      console.log(dateOg)
 
       let newDate = [dateOg[1], dateOg[2], dateOg[0]] //reformatting dateOg into US convention
       newDate = newDate.join('/')
-      console.log( newDate)
 
       let tryDate = new Date(dateOg[0], dateOg[1]-1, dateOg[2]) //eventually may try to parse weekday for display
       let dateInfo = tryDate.toLocaleDateString('en-US', {
         month: 'long',
         weekday: 'long',
       })
-
-      console.log(tryDate)
-      
       
       if( data.media_type === "image"){
         if( document.querySelector('#apodImg').classList.contains('hidden')){
@@ -57,14 +52,16 @@ function picOfDay(){
 //function for fetching historical APOD's
 function getDate(){
 
-  let date = document.querySelector('input').value
+  let date = document.querySelector('input').value //for inserting input value into fetch request string
   date = "date=" + date
 
   fetch(`https://api.nasa.gov/planetary/apod?${date}&api_key=aYmOC1DdrReJnOfgEjvWUgf8Du8wXR1VATNKFiRj`)
     .then( res => res.json() )
     .then( data => {
-      console.log(data)
-      console.log(date)
+      // console.log(data)
+
+      let dateOg = data.date.split('-') //original date format split into an array
+      let newDate = [dateOg[1], dateOg[2], dateOg[0]] //reformatting dateOg into US convention
       
       if( !data.media_type ){ 
         //if no media type, then given date is out of range, alert and load today's picture
@@ -91,6 +88,7 @@ function getDate(){
         document.querySelector('#apodImg').classList.add('hidden')
 
       }
+      document.querySelector('#apodDate').innerText = `${newDate}`
       document.querySelector('#apodName').innerText = `"${data.title}"`
       document.querySelector('#apodDescription').innerText = data.explanation
     })
